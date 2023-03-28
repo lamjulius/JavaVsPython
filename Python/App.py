4a0d0b1d37f3e2203cbbc54171f19a51470a90b8
Julius Lam <julius.lam2@gmail.com>
2023-03-28 16:55:49 +0200

    Changes to App.py

--- Python/App.py
+++ Python/App.py
@@ -1,3 +1,5 @@
+import time
+
 def sortList(xs):
     # Insertion Sort
     for i in range(1, len(xs)):
@@ -7,12 +9,26 @@ def sortList(xs):
             j -= 1
 
 def main():
-    with open("data1.txt", "r") as file:
+    with open("Common/data1.txt", "r") as file:
         data = file.read().split()
         data = [int(d) for d in data]
     
-    sortList(data)
-    print(data)
+    file1 = open("Python/pyresult.txt","a")
+   
+    
+    copy = []
+    for i in range(len(data)):
+        ints = data[i]
+        copy.append(ints)
+
+    for i in range(10):
+        print(copy)
+        t0 = time.time_ns()
+        sortList(copy)
+        t1 = time.time_ns()
+        time1 = t1 - t0
+        print(copy)
+        file1.write(str(i + 1) + "," + str(time1) + "\n")
     
 
 if __name__ == "__main__":
